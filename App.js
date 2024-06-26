08e1fa90177cba41a33ce52852ed541150d83210
ChiragParmar2111 <144097962+ChiragParmar2111@users.noreply.github.com>
2024-06-26 21:12:20 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,40 +1,37 @@
-import React, { useState } from 'react';
 
-const SearchComponent = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [items, setItems] = useState([
-    { id: 1, name: 'Banana' },
-    { id: 2, name: 'Apple' },
-    { id: 3, name: 'Orange' },
-    { id: 4, name: 'Mango' },
-    { id: 5, name: 'Pineapple' },
-    { id: 6, name: 'Watermaleon' },
-  ]);
+import { useState } from 'react';
 
-  const handleInputChange = (event) => {
-    setSearchTerm(event.target.value);
+function App() {
+  let [count, setCount] = useState(0); // usestate always two value return do.(array ) and your variable namre
+  const Increment = () => {
+    if (count >= 15) {
+      setCount(count);
+    } else {
+      setCount(count + 1);
+    }
+  };
+  const Decrement = () => {
+    if (count <= 0) {
+      setCount(count);
+
+    } else {
+      setCount(count - 1);
+    }
+  };
+  const reset = () => {
+    setCount(count = 0);
   };
 
-  // Function to filter items based on search term
-  const filteredItems = items.filter(item =>
-    item.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
   return (
     <div>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={handleInputChange}
-        placeholder="Enter search items..."
-      />
-      <ul>
-        {filteredItems.map(item => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
+      <h1>Count:{count}</h1>
+      <button onClick={Increment}>Increment</button>
+      <button onClick={Decrement}>Decrement</button>
+      <button onClick={reset}>reset</button>
     </div>
   );
-};
 
-export default SearchComponent;
+}
+
+export default App;
